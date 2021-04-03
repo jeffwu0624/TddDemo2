76038755d328e86ecac3cfa3c309d5076b9a53f4
jeffwu0624 <jeffwu0624@gmail.com>
2021-04-04 00:14:18 +0800

    create unit test project and BlackCatTest class

---
+++ WebApplication1.UnitTests/BlackCatTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace WebApplication1.UnitTests
+{
+    [TestFixture()]
+    public class BlackCatTests
+    {
+        [Test()]
+        public void CalculatedTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetCompanyNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [Test()]
+        public void GetFeeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
