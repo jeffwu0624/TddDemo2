04578c1c6c6d4b7803ba881daf328710ae0f1036
JeffWu0624 <jeffwu0624@hotmail.com>
2021-04-05 23:19:26 +0800

    fill Hsinchu Calculated and crate Hsinchu Unit Tests

---
+++ WebApplication1.UnitTests/HsinchuTests.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+
+namespace WebApplication1.UnitTests
+{
+    [TestFixture()]
+    public class HsinchuTests
+    {
+        private Hsinchu _hsinchu;
+
+        [SetUp]
+        public void Init()
+        {
+            _hsinchu = new Hsinchu();
+        }
+
+        [Test()]
+        public void GetCompanyName_Test()
+        {
+            var companyName = _hsinchu.GetCompanyName();
+
+            Assert.That(companyName, Is.EqualTo("新竹貨運"));
+        }
+
+        [Test()]
+        public void GetFee_ShouldBeZero_WhenInit()
+        {
+            var fee = _hsinchu.GetFee();
+
+            Assert.That(fee, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void GetFee_ShouldBe1830_WhenLengthMoreThen100()
+        {
+            _hsinchu.ShipProduct = new Product()
+            {
+                Length = 30,
+                Width = 20,
+                Height = 10,
+                Weight = 10
+            };
+
+            _hsinchu.Calculated();
+
+            var fee = _hsinchu.GetFee();
+
+            Assert.That(fee, Is.EqualTo(254).Within(0.9));
+        }
+    }
+}
--- WebApplication1/Index.aspx.cs
+++ WebApplication1/Index.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace WebApplication1
 {
@@ -145,21 +146,54 @@ namespace WebApplication1
 
     public class Hsinchu
     {
+        private readonly string _companyName = "新竹貨運";
+        private double _fee = 0;
+
         public void Calculated()
         {
-            throw new NotImplementedException();
+            //lblLogistics.Text = ddlLogistics.SelectedItem.Text;
+
+            //var width = Convert.ToDouble(txtWidth.Text);
+            //var length = Convert.ToDouble(txtLength.Text);
+            //var height = Convert.ToDouble(txtHeight.Text);
+
+            //var size = (width * length * height);
+
+            //if (length > 100 || width > 100 || height > 100)
+            //{
+            //    lblFee.Text = (size * 0.0000353 * 1100 + 500).ToString("C");
+            //}
+            //else
+            //{
+            //    lblFee.Text = (size * 0.0000353 * 1200).ToString("C");
+            //}
+
+            var width = ShipProduct.Width;
+            var length = ShipProduct.Length;
+            var height = ShipProduct.Height;
+
+            var size = (width * length * height);
+
+            if (length > 100 || width > 100 || height > 100)
+            {
+                _fee = (size * 0.0000353 * 1100 + 500);
+            }
+            else
+            {
+                _fee = (size * 0.0000353 * 1200);
+            }
         }
 
         public Product ShipProduct { get; set; }
 
         public string GetCompanyName()
         {
-            throw new NotImplementedException();
+            return _companyName;
         }
 
         public double GetFee()
         {
-            throw new NotImplementedException();
+            return _fee;
         }
     }
 
